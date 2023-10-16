75e2a04c41954de7222ea80259bb70f9c2251770
Magali <77433765+magalidugue@users.noreply.github.com>
2023-10-16 07:44:26 +0200

    server and database init

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-
+const mysql = require("mysql");
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,10 +11,25 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "SDVITRAIL",
+  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log("Erreur de connection : " + err);
+    return;
+  }
+  console.log("Connexion réussi à la bdd");
+});
 
 app.get("/", (_req, res) => {
   res.send("Sébastien");
